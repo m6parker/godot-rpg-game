ef5d842fb29f63c60ece570570f27a5cda795e89
Mary Parker <98114662+m6parker@users.noreply.github.com>
2026-06-23 22:36:07 -0400

    fixed skill tree ui, interactivity

--- src/ui/menus/skill_tree.gd
+++ src/ui/menus/skill_tree.gd
@@ -52,7 +52,6 @@ func create_branch_column(branch_name: String, skills: Array) -> void:
 	
 	var title_label = Label.new()
 	title_label.text = upper_branch_name
-	# title_label.text = upper_branch_name + "\n(" + str(current_player_points) + " point available)"
 	title_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
 	column.add_child(title_label)
 	
@@ -61,7 +60,7 @@ func create_branch_column(branch_name: String, skills: Array) -> void:
 	for i in range(skills.size()):
 		var skill_name = skills[i] as String
 		var skill_button = Button.new()
-		skill_button.text = skill_name
+		skill_button.text = skill_name.capitalize()
 		skill_button.custom_minimum_size = Vector2(120, 40)
 		
 		var cost_to_upgrade = 1 if i == 0 else i * 5
@@ -70,31 +69,44 @@ func create_branch_column(branch_name: String, skills: Array) -> void:
 		
 		if is_already_purchased:
 			skill_button.disabled = true
-			#skill_button.text = skill_name
 			skill_button.self_modulate = Color.GREEN
-		elif previous_skill_purchased and has_enough_points:
-			skill_button.disabled = false
+		elif previous_skill_purchased:
+			if has_enough_points:
+				skill_button.disabled = false
+				skill_button.self_modulate = Color.WHITE
+			else:
+				skill_button.disabled = true
+				skill_button.self_modulate = Color(1, 1, 1, 0.4)
 		else:
 			skill_button.disabled = true
+			skill_button.self_modulate = Color.TRANSPARENT
 				
 		skill_button.pressed.connect(func(): _on_skill_clicked(upper_branch_name, skill_name))
 		column.add_child(skill_button)
 		
+		# connecting branch lines
 		if i < skills.size() - 1:
 			var line = Line2D.new()
 			line.width = 2
+			
 			var next_skill_cost = (i + 1) * 5
 			var next_skill_affordable = current_player_points >= next_skill_cost
-			# connector line will only be green is the skill is already unlocked and the next one is available
-			if is_already_purchased and next_skill_affordable:
-				line.default_color = Color.GREEN
+			
+			if is_already_purchased:
+				line.self_modulate = Color.WHITE
+				if next_skill_affordable:
+					line.default_color = Color.GREEN
+				else:
+					line.default_color = Color.DARK_GRAY
 			else:
-				line.default_color = Color.DARK_GRAY
+				line.self_modulate = Color.TRANSPARENT
+				
 			line.add_point(Vector2(60, 40))
 			line.add_point(Vector2(60, 60))
 			skill_button.add_child(line)
 			
 		previous_skill_purchased = is_already_purchased
+
 
 # skill button pressed
 func _on_skill_clicked(branch: String, skill_name: String) -> void:
