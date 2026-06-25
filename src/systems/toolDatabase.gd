63c85986a96cd6ff0a742495c6b7ffae2aba19f2
Mary Parker <98114662+m6parker@users.noreply.github.com>
2026-06-24 20:25:48 -0400

    added tools

---
+++ src/systems/toolDatabase.gd
@@ -0,0 +1,59 @@
+extends Node
+
+var items: Dictionary = {}
+
+func _ready() -> void:
+	load_items_from_json("res://data/tools.json")
+
+func load_items_from_json(file_path: String) -> void:
+	if not FileAccess.file_exists(file_path):
+		push_error("json file not found at: " + file_path)
+		return
+		
+	var file = FileAccess.open(file_path, FileAccess.READ)
+	var json_text = file.get_as_text()
+	file.close()
+	
+	#parse the json data into readable text
+	var raw_data = JSON.parse_string(json_text)
+	if raw_data == null:
+		push_error("error parsing")
+		return
+		
+	# loop thru the text and pull data
+	for item_data in raw_data:
+		var new_item = ItemData.new()
+		
+		new_item.item_name = item_data["name"]
+		new_item.description = item_data["description"]
+		new_item.price = item_data.get("price", 0)
+		
+		if item_data.has("type"):
+			new_item.item_type = item_data["type"]
+		else:
+			new_item.item_type = "misc"
+		
+		# formatting
+		var clean_file_name = new_item.item_name.to_lower().replace(" ", "_")
+		var expected_path = "res://assets/tools/" + clean_file_name + ".png"
+		#expected_path = "res://assets/items/" +  new_item.item_type.to_lower() + "/" + clean_file_name + ".png"
+		#print("expected path: ", expected_path)
+		
+		if ResourceLoader.exists(expected_path):
+			new_item.item_texture = load(expected_path)
+		else:
+			# default to ho image if the png image cannot be found
+			new_item.item_texture = load("res://assets/tools/hoe.png")
+			print("failed to find path '", expected_path, "' for item: '", new_item.item_name, "'")
+			
+		items[item_data["name"]] = new_item
+
+
+func get_tools_in_category(category: String) -> Dictionary:
+	var filtered_items: Dictionary = {}
+	for item_name in items:
+		var item_data = items[item_name]
+		# if item is in given category
+		if item_data.item_type.to_lower() == category.to_lower():
+			filtered_items[item_name] = item_data
+	return filtered_items
